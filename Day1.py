fcdea5944657f2e5da5bbb9c51b383dbccae6628
santhoshworkspace.gmail.com <santhoshworkspace@gmail.com>
2025-05-28 09:42:33 +0530

    Day1 morning

--- Day1.py
+++ Day1.py
@@ -1,5 +1,13 @@
 import pandas as pd
 df=pd.read_csv('./weather_jan_2016.csv')
 print(df)
-Temperature=df['Temperature']
-print("Temperature",Temperature)
+print(df['Temperature'])
+print(df['EST'][df['Events']=='Rain'])
+df.fillna("0",inplace=True)
+print(df)
+print(df.shape)
+rows,columns=df.shape
+print(rows,columns)
+print(df.head(10))
+print(df.tail(10))
+print(df[3:7])
